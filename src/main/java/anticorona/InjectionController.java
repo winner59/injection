05886602ae47d3801c4a48bbe9937679b056b3ee
winner59 <82795860+winner59@users.noreply.github.com>
2021-06-24 22:36:06 +0900

    Update InjectionController.java
    발급가능여부추가

--- src/main/java/anticorona/InjectionController.java
+++ src/main/java/anticorona/InjectionController.java
@@ -5,12 +5,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
- 
-@RestController
-public class InjectionController {
+ @RestController
+ public class InjectionController {
 
-    }
+     @Autowired
+     InjectionRepository injectionRepository;
+
+     @RequestMapping(value = "/injections/checkStatus",
+        method = RequestMethod.GET,
+        produces = "application/json;charset=UTF-8")
+    public boolean checkAndBookStock(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("##### /injection/checkStaus  called #####");
+
+        boolean status = false;
+
+        Long bookingId = Long.valueOf(request.getParameter("bookingId"));
+        
+        Optional<Injection> injection = injectionRepository.findById(bookingId);
+        if(injection .isPresent()){
+            Injection injectionValue = injection.get();
+
+            //증명서 발급 가능한지 체크 
+            if(injectionValue.getStatus() =="INJECTION_COMPLETED") {
+                //발급  가능하면 예약수량 증가
+                status = true;
+                //injectionValue.setStatus(injectionValue.getBookQty() + 1);
+                injectionRepository.save(injectionValue);
+            }
+        }
+
+        return status;
+     }
+ }
